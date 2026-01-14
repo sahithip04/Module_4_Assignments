8581756fd02a5dcb4bf6c22be14ade72a20acf6c
sahithip04 <palparthisahithireddy@gmail.com>
2026-01-14 19:27:19 +0530

    express routing is done for users and todos

---
+++ Express/src/routes/todos.routes.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const router = express.Router();
+const dbPath = path.join(__dirname, "..", "db.json");
+
+const readDB = () => {
+  return JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+};
+
+const writeDB = (data) => {
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+};
+
+router.post("/add", (req, res) => {
+  const db = readDB();
+
+  const newTodo = {
+    id: Date.now(),
+    completed: false,
+    ...req.body
+  };
+
+  db.todos.push(newTodo);
+  writeDB(db);
+
+  res.status(201).json(newTodo);
+});
+
+router.get("/", (req, res) => {
+  const db = readDB();
+  res.status(200).json(db.todos);
+});
+
+
+router.get("/:todoId", (req, res) => {
+  const db = readDB();
+  const todo = db.todos.find(t => t.id == req.params.todoId);
+
+  if (!todo) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  res.status(200).json(todo);
+});
+
+
+router.put("/update/:todoId", (req, res) => {
+  const db = readDB();
+  const index = db.todos.findIndex(t => t.id == req.params.todoId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  db.todos[index] = {
+    ...db.todos[index],
+    ...req.body
+  };
+
+  writeDB(db);
+  res.status(200).json(db.todos[index]);
+});
+
+router.delete("/delete/:todoId", (req, res) => {
+  const db = readDB();
+  const initialLength = db.todos.length;
+
+  db.todos = db.todos.filter(t => t.id != req.params.todoId);
+
+  if (db.todos.length === initialLength) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  writeDB(db);
+  res.status(200).json({ message: "Todo deleted successfully" });
+});
+
+module.exports = router;
